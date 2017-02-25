77ad468e62786abc41712a5e23a3d9611be3e52f
Steve Hansen Smythe <hansensmythe@gmail.com>
2017-02-25 13:08:03 -0700

    Google account update
    Replaced old GA functions and account number with new one.

--- js/widget.js
+++ js/widget.js
@@ -1,17 +1,22 @@
 // Used by widget.html to open a separate window for SoundClick
 
 // Google Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-1619574-2']);
-_gaq.push(['_trackPageview']);
-(function() {
-	var ga = document.createElement('script');
-	ga.type = 'text/javascript';
-	ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(ga, s);
-})();
+
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] ||
+  function() {
+    (i[r].q = i[r].q || []).push(arguments);
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+  m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga'); 
+
+ga('create', 'UA-1619574-4', 'auto');
+ga('send', 'pageview');
 
 function writeFlashvars() {
 	var songid = gup('sid');
