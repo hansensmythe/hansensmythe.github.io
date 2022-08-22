f39647771e7e73c1008a590a1b95632eda05b869
Steve Hansen Smythe <hansensmythe@gmail.com>
2022-08-22 06:29:19 -0700

    Total redo of qwirkle
    Reoriented setup.html properly - had the boards oriented the wrong way before. Rewrote setup.js. Renamed all the tiles with descriptive names.

--- qwirkle/js/setup.js
+++ qwirkle/js/setup.js
@@ -1,231 +1,175 @@
-var qwirkleSize = 6;
-var colours = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple'];
-var shapes = ['Circle', 'Diamond', 'Flower', 'Nunchuck', 'Square', 'Star'];
-var tiles = {};
-var isImageEnabled = true;
-var maxTileAttempts = 10;
-var maxFillAttempts = 25;
-var updateDate = "21 April 2015";
+// These colours and shapes must correspond with the filenames in the tiles folder
+// e.g. tiles/RedCircle.png is found by concatenating COLOURS[0] and SHAPES[0]
+const COLOURS = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple'];
+const SHAPES = ['Circle', 'Diamond', 'Eightstar', 'Flower', 'Fourstar', 'Square'];
+const USEIMAGES = true;
+const MAXFILLS = 15;
+const UPDATEDATE = "22 August 2022";
 
-function Tile(c, s) {
-  this.c = c;
-  this.s = s;
-  this.isAvailable = true;
-}
-
-Tile.prototype.toString = function() {
-  return colours[this.c] + ' ' + shapes[this.s];
-};
-
-Tile.prototype.getElement = function() {
-  // returns element representing this tile
-  var tileElement;
-  if (isImageEnabled) {
-    tileElement = document.createElement("img");
-    tileElement.src = "tiles/t" + this.c + this.s + ".png";
-  } else {
-    tileElement = document.createTextNode(this.toString());
-  }
-  return tileElement;
-};
-
-function getColourIndex(qwirkle, c) {
-  var colourIndex = -1;
-  for (var i = 0; i < qwirkle.length && colourIndex === -1; i++) {
-    if (qwirkle[i].c === c) {
-      colourIndex = i;
-    };
-  }
-  return colourIndex;
-}
-
-function getShapeIndex(qwirkle, s) {
-  var shapeIndex = -1;
-  for (var i = 0; i < qwirkle.length && shapeIndex === -1; i++) {
-    if (qwirkle[i].s === s) {
-      shapeIndex = i;
-    };
-  }
-  return shapeIndex;
-}
-
-function getQwirkleIndex(tile) {
-  var qwirkleIndex = -1;
-  for (var q = 0; q < qwirkles.length && qwirkleIndex === -1; q++) {
-    qwirkleIndex = qwirkles[q].indexOf(tile);
-  }
-  return qwirkleIndex;
-}
-
-function findRandomTile(qwirkle, availableColours, availableShapes) {
-  // If the qwirkle is empty, we just need to find one that is not already used anywhere.
-  // If the qwirkle already has members, make sure the new one creates no duplicates
-  if (!availableColours) {
-    availableColours = [];
-    for (var i = 0; i < qwirkleSize; i++) {
-      if (getColourIndex(qwirkle, i) === -1) {
-        availableColours.push(i);
-      }
+class Tile {
+    constructor(colour, shape) {
+        this.colour = colour;
+        this.shape = shape;
+        this.isUsed = false;
     }
-  }
-  if (!availableShapes) {
-    availableShapes = [];
-    for (var i = 0; i < qwirkleSize; i++) {
-      if (getShapeIndex(qwirkle, i) === -1) {
-        availableShapes.push(i);
-      }
+    setUsed() {
+        this.isUsed = true;
     }
-  }
-  var newTile = null;
-  if (availableColours.length === 1 && availableShapes === 1) {
-    // we don't need random - we know exactly what it should be.
-    var targetTile = tiles[availableColours[0]][availableShapes[0]];
-    if (targetTile.isAvailable) {
-      newTile = targetTile;
-    } else {
-      // All right! Who has my tile? Can we trade? So long as it's not the 1st or 2nd item, it should be safe
-      // However, this code never seems to succeed. TODO: analyze why and fix it.
-      /*
-      var qwirkleIndex = getQwirkleIndex(targetTile);
-      var tileIndex = qwirkles[qwirkleIndex].indexOf(targetTile);
-      if (tileIndex > 1) {
-        qwirkles[qwirkleIndex].splice(tileIndex, 1);
-        console.log("I stole " + targetTile.toString() + " from " + qwirkles[qwirkleIndex].toString());
-        newTile = targetTile;
-        if (!fillQwirkle(qwirkles[qwirkleIndex])) {
-          console.log("I tried stealing a tile, but then I couldn't finish the qwirkle from which I stole it.");
-          return null;
+    isAvailable() {
+        return !this.isUsed;
+    }
+    toString() {
+        return `${this.colour}${this.shape}`;
+    }
+    getElement() {
+        // Returns element representing this tile
+        let tileElement;
+        if (USEIMAGES) {
+            tileElement = document.createElement("img");
+            tileElement.src = `tiles/${this.toString()}.png`;
+        } else {
+            tileElement = document.createTextNode(this.toString());
         }
-      }
-      */
+        return tileElement;
     }
-  } else {
-    // The more populated the qwirkle is, the narrower the choices. If we don't have an
-    // available tile in maxTileAttempts tries, there's probably a programming error and we should give up.
-    for (var i = 0; i < maxTileAttempts && !newTile; i++) {
-      var randomC = availableColours[Math.floor(Math.random() * availableColours.length)];
-      var randomS = availableShapes[Math.floor(Math.random() * availableShapes.length)];
-      if (tiles[randomC][randomS].isAvailable) {
-        // so far so good, but does it work with what we already have?
-        newTile = tiles[randomC][randomS];
-      }
-    }
-  }
-  return newTile;
 }
 
-function consumeTile(qwirkle, tile) {
-  if (tile.isAvailable) {
-    qwirkle.push(tile);
-    tile.isAvailable = false;
+class BagOfTiles {
+    constructor() {
+        this.tiles = [];
+        COLOURS.forEach((colour) => {
+            SHAPES.forEach((shape) => {
+                this.tiles.push(new Tile(colour, shape));
+            });
+        });
+    }
+    getAvailableTiles() {
+        return this.tiles.filter((tile) => tile.isAvailable());
+    }
+    getRandomTile(availableColours, availableShapes) {
+        // From the available tiles, further restrict the possibilities to conform to the provided arrays of available values
+        const availableTiles = this.getAvailableTiles().filter((tile) => availableColours.includes(tile.colour) && availableShapes.includes(tile.shape));
+        if (availableTiles.length === 0) {
+            throw new Error('Ran out of legal tiles');
+        }
+        const randomIndex = Math.floor(Math.random() * availableTiles.length);
+        const drawnTile = availableTiles[randomIndex];
+        drawnTile.setUsed();
+        return drawnTile;
+    }
+}
+
+class Qwirkle {
+    constructor(index) {
+        this.index = index;
+        this.tiles = {};
+    }
+    getAvailableColours() {
+        const coloursInThisQwirkle = Object.values(this.tiles).map((tile) => tile.colour);
+        return COLOURS.filter((colour) => !coloursInThisQwirkle.includes(colour));
+    }
+    getAvailableShapes() {
+        const shapesInThisQwirkle = Object.values(this.tiles).map((tile) => tile.shape);
+        return SHAPES.filter((shape) => !shapesInThisQwirkle.includes(shape));
+    }
+    addTile(position, drawnTile) {
+        this.tiles[position] = drawnTile;
+    }
+    addRandomTile(bagOfTiles, position) {
+        return this.addTile(position, bagOfTiles.getRandomTile(this.getAvailableColours(), this.getAvailableShapes()));
+    }
+    addTileOfColour(bagOfTiles, position, colour) {
+        return this.addTile(position, bagOfTiles.getRandomTile([colour], this.getAvailableShapes()));
+    }
+    addTileOfShape(bagOfTiles, position, shape) {
+        return this.addTile(position, bagOfTiles.getRandomTile(this.getAvailableColours(), [shape]));
+    }
+    populateBoard() {
+        Object.keys(this.tiles).forEach((position) => {
+            // For each position, find the matching spot on the board.
+            // For example, quirkle index=2 with tile position=3 would retrieve document element q23
+            const tileElement = this.tiles[position].getElement();
+            const cell = document.getElementById(`q${this.index}${position}`);
+            cell.appendChild(tileElement);
+        });
+    }
+}
+
+/**
+ * 
+ * @param {number} tryCount - Used only for logging a message so that we can document how many times we've tried
+ * @returns 
+ */
+function fill(tryCount) {
+    const bagOfTiles = new BagOfTiles();
+    // Create qwirkles starting from the top, going clockwise, with one qwirkle per quadrant, where the last two tiles of the previous
+    // qwirkle complement the first two tiles of the next qwirkle. Try to get the quirkles connected up, then try to fill the middles.
+    // If at any time we run out of legal tiles, an error will be thrown and we stop and try again.
+    try {
+        const qNE = new Qwirkle(0);
+        qNE.addRandomTile(bagOfTiles, 0);
+        qNE.addRandomTile(bagOfTiles, 1);
+        qNE.addRandomTile(bagOfTiles, 4);
+        qNE.addRandomTile(bagOfTiles, 5);
+
+        const qSE = new Qwirkle(1);
+        qSE.addTileOfColour(bagOfTiles, 0, qNE.tiles[5].colour);
+        qSE.addTileOfColour(bagOfTiles, 1, qNE.tiles[4].colour);
+        qSE.addRandomTile(bagOfTiles, 4);
+        qSE.addRandomTile(bagOfTiles, 5);
+
+        const qSW = new Qwirkle(2);
+        qSW.addTileOfShape(bagOfTiles, 0, qSE.tiles[5].shape);
+        qSW.addTileOfShape(bagOfTiles, 1, qSE.tiles[4].shape);
+        qSW.addRandomTile(bagOfTiles, 4);
+        qSW.addRandomTile(bagOfTiles, 5);
+
+        const qNW = new Qwirkle(3);
+        qNW.addTileOfColour(bagOfTiles, 0, qSW.tiles[5].colour);
+        qNW.addTileOfColour(bagOfTiles, 1, qSW.tiles[4].colour);
+        qNW.addTileOfShape(bagOfTiles, 4, qNE.tiles[1].shape);
+        qNW.addTileOfShape(bagOfTiles, 5, qNE.tiles[0].shape);
+
+        // We've defined the circle of qwirkles! Fill in the middle tiles
+        qNE.addRandomTile(bagOfTiles, 2);
+        qNE.addRandomTile(bagOfTiles, 3);
+        qSE.addRandomTile(bagOfTiles, 2);
+        qSE.addRandomTile(bagOfTiles, 3);
+        qSW.addRandomTile(bagOfTiles, 2);
+        qSW.addRandomTile(bagOfTiles, 3);
+        qNW.addRandomTile(bagOfTiles, 2);
+        qNW.addRandomTile(bagOfTiles, 3);
+
+        qNE.populateBoard();
+        qSE.populateBoard();
+        qSW.populateBoard();
+        qNW.populateBoard();
+    } catch (err) {
+        console.log(`Error: ${err.message} on try ${tryCount}`);
+        return false;
+    }
+
+    // If we got here, we successfully filled the quirkles
+    const unusedElement = document.getElementById("unused");
+    unusedElement.appendChild(document.createTextNode("Unused tiles: "));
+    const unusedTiles = bagOfTiles.getAvailableTiles();
+    unusedTiles.forEach((unusedTile) => unusedElement.appendChild(unusedTile.getElement()));
+    const updatedElement = document.getElementById("updated");
+    updatedElement.innerHTML = UPDATEDATE;
     return true;
-  } else {
-    return false;
-  }
-}
-
-function fillQwirkle(qwirkle) {
-  var isValid = true;
-  while (qwirkle.length < qwirkleSize && isValid) {
-    var newTile = findRandomTile(qwirkle);
-    if (newTile) {
-      isValid = consumeTile(qwirkle, newTile);
-    } else {
-      isValid = false;
-    }
-  }
-  return isValid;
-}
-
-function forbidKittycorners(tempArray, qwirkle) {
-  [tiles[qwirkle[0].c][qwirkle[1].s], tiles[qwirkle[1].c][qwirkle[0].s]].forEach(function(tile) {
-    if (tile.isAvailable) {
-      tempArray.push(tile);
-      tile.isAvailable = false;
-    }
-  });
-}
-
-function fill() {
-  for (var c = 0; c < colours.length; c++) {
-    tiles[c] = {};
-    for (var s = 0; s < shapes.length; s++) {
-      tiles[c][s] = new Tile(c, s);
-    }
-  }
-
-  var qwirkles = [[], [], [], []];
-  var temporarilyUnavailable = [];
-  // Fill the first two tiles of each qwirkle, making sure they allow a cenral qwirkle
-  consumeTile(qwirkles[0], findRandomTile(qwirkles[0]));
-  consumeTile(qwirkles[0], findRandomTile(qwirkles[0]));
-  forbidKittycorners(temporarilyUnavailable, qwirkles[0]);
-  // The second qwirkle should match the first by colour
-  consumeTile(qwirkles[1], findRandomTile(qwirkles[1], [qwirkles[0][0].c]));
-  consumeTile(qwirkles[1], findRandomTile(qwirkles[1], [qwirkles[0][1].c]));
-  forbidKittycorners(temporarilyUnavailable, qwirkles[1]);
-  // The third qwirkle should match the first by shape
-  consumeTile(qwirkles[2], findRandomTile(qwirkles[2], null, [qwirkles[0][0].s]));
-  consumeTile(qwirkles[2], findRandomTile(qwirkles[2], null, [qwirkles[0][1].s]));
-  // The fourth qwirkle starts with two tiles we know exactly
-  consumeTile(qwirkles[3], tiles[qwirkles[2][0].c][qwirkles[1][0].s]);
-  consumeTile(qwirkles[3], tiles[qwirkles[2][1].c][qwirkles[1][1].s]);
-
-  temporarilyUnavailable.forEach(function(tile) {
-    tile.isAvailable = true;
-  });
-
-  // We need to get completely valid qwirkles before we start drawing to the screen. Easier that than clearing and repopulating
-  var isValid = true;
-  qwirkles.forEach(function(qwirkle) {
-    if (!fillQwirkle(qwirkle)) {
-      isValid = false;
-    }
-  });
-
-  if (isValid) {
-    // Draw images to table. We don't use forEach because we need the indexes to find the corresponding element
-    for (var i = 0; i < qwirkles.length; i++) {
-      isValid = fillQwirkle(qwirkles[i]);
-      for (var j = 0; j < qwirkles[i].length; j++) {
-        var tile = qwirkles[i][j];
-        var tileElement = tile.getElement();
-        var cell = document.getElementById('q' + i + j);
-        cell.appendChild(tileElement);
-      }
-    }
-    finish();
-    return true;
-  } else {
-    console.log("Failed to fill qwirkle - last tile was both necessary and unavailable");
-  }
-}
-
-function finish() {
-  var unused = document.getElementById("unused");
-  unused.appendChild(document.createTextNode("Unused tiles: "));
-  for (var c = 0; c < colours.length; c++) {
-    for (var s = 0; s < shapes.length; s++) {
-      if (tiles[c][s].isAvailable) {
-        unused.appendChild(tiles[c][s].getElement());
-      }
-    }
-  }
-  var updated = document.getElementById("updated");
-  updated.innerHTML = updateDate;
 }
 
 // Called from setup.html
 function loadQwirkleConnect() {
-  // try maxFillAttempts times. If it's not possible in that many, there's probably a programming error and we should stop trying
-  var isInitialized = false;
-  for (var i = 0; i < maxFillAttempts && !isInitialized; i++) {
-    console.log("Populating Qwirkle Connect board (try #" + (i + 1) + ")");
-    isInitialized = fill();
-  }
-  if (!isInitialized) {
-    var unused = document.getElementById("unused");
-    var message = document.createTextNode("We came, we saw, but we failed to conquer. Check console log for error.");
-    unused.appendChild(message);
-  }
+    // try MAXFILLS times. If it's not possible in that many, there's probably a programming error and we should stop trying
+    let isInitialized = false;
+    for (let tryCount = 1; tryCount <= MAXFILLS && !isInitialized; tryCount++) {
+        isInitialized = fill(tryCount);
+    }
+    if (!isInitialized) {
+        const unused = document.getElementById("unused");
+        const message = document.createTextNode("We came, we saw, but we failed to conquer. Check console log for error.");
+        unused.appendChild(message);
+    }
 }
+  
